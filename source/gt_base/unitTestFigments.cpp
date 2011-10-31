f5bde38e29cba98101438d22b879cd19381a2a73
stuandlou <stuandlou@stu-ans-lous-macbook-pro.local>
2011-10-31 20:59:00 +0000

    [fix] Added some missing files. [tidy] remove old visual studio project. [tidy] removed yet another ds store file.

---
+++ source/gt_base/unitTestFigments.cpp
@@ -0,0 +1,32 @@
+
+#include "unitTestFigments.hpp"
+
+#if defined(DEBUG) && defined(GTUT)
+	using namespace gt;
+
+	const cPlugTag *cTestNum::xPT_num = tOutline<cTestNum>::makePlugTag("number");
+
+	const cCommand::dUID	cTestNum::xGetData = tOutline<cTestNum>::makeCommand(
+		"getData", &cTestNum::patGetData, cTestNum::xPT_num, NULL
+	);
+
+	cTestNum::cTestNum() : myNum(42) {
+	}
+
+	cTestNum::~cTestNum(){
+	}
+
+	void cTestNum::save(cByteBuffer* pAddHere) {
+		myNum.save(pAddHere);
+	}
+
+	void cTestNum::loadEat(cByteBuffer* pBuff, dReloadMap* pReloads) {
+		myNum.loadEat(pBuff, pReloads);
+	}
+
+	void cTestNum::patGetData(ptrLead aLead){
+		aLead->addPlug(&myNum, xPT_num);
+	}
+
+#endif
+
