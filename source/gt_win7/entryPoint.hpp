485a0bbf861796fd4c228afbeb05eda0e5a89353
stuandlou <stuandlou@Stuart-Bridgenss-MacBook-Pro.local>
2013-02-05 22:30:48 +0000

    Added the new entry point strategy for managing how programs are started.

---
+++ source/gt_win7/entryPoint.hpp
@@ -0,0 +1,12 @@
+/*
+ * !\file	entryPoint.hpp
+ * !\brief	Include this and use the macro to define the entry function for your program.
+ */
+
+#ifndef ENTRYPOINT_HPP
+#define ENTRYPOINT_HPP
+
+
+#define ENTRYPOINT int main(void)
+
+#endif
